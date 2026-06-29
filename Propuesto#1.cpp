97db091425746cc4f608312415fe81ec554fd559
azegarrah <azegarrah@unjbg.edu.pe>
2026-06-29 16:16:09 -0500

    Propuesto#1: Se puso en el bloque main la variable n y su entrada, para crear el arreglo emp con n espacios

--- Propuesto#1.cpp
+++ Propuesto#1.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
 
-struct Empleado {
+struct Empleado{
     string nombre;
     char sexo;
     double sueldo;
 };
+int main(){
+    int n;
+    cin >> n;
+    Empleado emp[n];
+
+    return 0;
+}
