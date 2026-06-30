bb40d8ad045db107b6fcd80c9ad91255d91ca133
azegarrah <azegarrah@unjbg.edu.pe>
2026-06-29 19:55:28 -0500

    Propuesto#2: Usamos un for para pedir a los n espacios del arreglo el nombre, dni y edad, también acumulamos la suma de la edad y usamos un if para saber si una persona es mayor a 50, para que incremente la variable mayores en uno por cada persona mayor a 50

--- Propuesto#2.cpp
+++ Propuesto#2.cpp
@@ -13,6 +13,15 @@ int main(){
 
     int mayores = 0;
     int suma = 0;
+    for(int i = 0; i < n; i++){
+        cin >> per[i].nombre;
+        cin >> per[i].dni;
+        cin >> per[i].edad;
+        suma = suma + per[i].edad;
 
+        if(per[i].edad > 50){
+            mayores++;
+        }
+    }
     return 0;
 }
