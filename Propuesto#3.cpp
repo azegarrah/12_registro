ba31bd3c5d26efcf0fc14847bf7824dc7eeb2974
azegarrah <azegarrah@unjbg.edu.pe>
2026-06-29 21:08:00 -0500

    Propuesto#3: Ponemos un if dentro del do para usar un for si el mesBuscado es diferente de cero el for recorre todas las personas del arreglo, para luego comparar el mes con el mesBuscado e imprimira el nombre/dia/mes/anio

--- Propuesto#3.cpp
+++ Propuesto#3.cpp
@@ -22,7 +22,17 @@ int main(){
     int mesBuscado;
     do{
         cin >> mesBuscado;
-
+        
+        if(mesBuscado != 0){
+            for(int i = 0; i < n; i++){
+                if(per[i].mes == mesBuscado){
+                    cout << per[i].nombre << " ";
+                    cout << per[i].dia << "/";
+                    cout << per[i].mes << "/";
+                    cout << per[i].anio << '\n';
+                }
+            }
+        }
     } while(mesBuscado != 0);
 
     return 0;
